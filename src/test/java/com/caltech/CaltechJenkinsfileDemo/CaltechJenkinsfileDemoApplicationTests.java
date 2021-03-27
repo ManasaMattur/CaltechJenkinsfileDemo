8c1fbf04cbab82de2f46916ca8c8da3ef63b75e3
student <manasa6687@gmail.com>
2021-03-27 17:14:24 +0000

    FirstCommit

---
+++ src/test/java/com/caltech/CaltechJenkinsfileDemo/CaltechJenkinsfileDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caltech.CaltechJenkinsfileDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaltechJenkinsfileDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
